fd2229eba708368542f8345ba2929ab92f76b3a3
jaklahaulps <liu.perry@gmail.com>
2018-12-03 23:37:03 +0000

    updated git ignore and download_avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,23 @@
 var request = require('request');
+var key = '0da5fa54714cb8998bceafdc95f59511031c34f8';
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      Authorization: 'token ' + key
+    }
+  }
 }
+
+request(options, function(errm res, body){
+  cb(err, body);
+});
 
 console.log('Welcome to the Github Avatar Downloader!');
 
-getRepoContributors(, "jquery", function(err, result){
+getRepoContributors("jquery", "jquery", function(err, result){
   console.log("Errors", err);
   console.log("Result:", result);
-});
+}
