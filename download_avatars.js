2dead2c8c6fa1d85c33e00ee161c4023217a19a5
jaklahaulps <liu.perry@gmail.com>
2018-12-04 02:17:27 +0000

    added requirement for input

--- download_avatars.js
+++ download_avatars.js
@@ -5,15 +5,20 @@ var proc = process.argv
 //var filepath = './avatars/'
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      Authorization: 'token ' + key
-    }}
-  request(options, function(err, res, body){
-    cb(err, JSON.parse(body));
-  });
+  if (repoOwner !== undefined && repoName !== undefined){
+
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        Authorization: 'token ' + key
+      }}
+    request(options, function(err, res, body){
+      cb(err, JSON.parse(body));
+    });
+    } else{
+    console.log('Requires input.')
+  };
 }
 
 function downloadImageByURL(url, filePath){
