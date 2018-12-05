4313a9996ae6693ee6ca53c1f17a1bfa82a720d7
jaklahaulps <liu.perry@gmail.com>
2018-12-05 01:45:07 +0000

    updated avatar code

--- download_avatars.js
+++ download_avatars.js
@@ -5,15 +5,16 @@ var proc = process.argv
 //var filepath = './avatars/'
 // console.log("env", process.env.DB_PASS)
 
-function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors( repoOwner, repoName, cb) {
   if (repoOwner !== undefined && repoName !== undefined) {
     var options = {
       url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
       headers: {
         'User-Agent': 'request',
         Authorization: process.env.DB_PASS
-      }}
-    request(options, function(err, res, body){
+      }
+    };
+    request(options, function(err, res, body) {
       cb(err, JSON.parse(body));
     });
   } else {
@@ -21,7 +22,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 };
 
-function downloadImageByURL(url, filePath){
+function downloadImageByURL(url, filePath) {
   request.get(url)               // Note 1
        .on('error', function (err) {
          throw err;
@@ -30,14 +31,23 @@ function downloadImageByURL(url, filePath){
        })
        .pipe(fs.createWriteStream(filePath));
 };
-console.log('Welcome to the Github Avatar Downloader!');
+console.log( 'Welcome to the Github Avatar Downloader!' );
 
-getRepoContributors(proc[2], proc[3], function(err, result){
+function envCheck(){
+  if (process.env.DB_PASS == false){
+    console.log('.env is invalid')
+    return;
+  };
+}
+
+envCheck()
+
+getRepoContributors(proc[2], proc[3], function(err, result) {
   for (var i = 0; i < result.length; i++ ){
     //console.log(result);
     var filePath = './avatarImg/' + result[i].login + '.jpg'; //avatar file path is not working for me
     var urlPaths = result[i].avatar_url;
     downloadImageByURL(urlPaths, filePath);
   }
-  console.log("Image download complete.")
+  //console.log("Image download complete.")
 });
